b8e36568ce0acef692bb21efafbc2a250ef224f0
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-07 17:34:56 -0800

    Created Login Component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./styles/App.css";
 
 class App extends Component {
 	render() {
-		return <div className="App" />;
+		return (
+			<div className="App">
+				<Router>
+					<Route path="/login" component={Login} />
+				</Router>
+			</div>
+		);
 	}
 }
 
---
+++ friends/src/components/Authentication/Login.js
@@ -0,0 +1,49 @@
+class Login extends Component {
+	state = {
+		username: "",
+		password: ""
+	};
+
+	handleChanges = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
+
+	loginUser = e => {
+		e.preventDefault();
+
+		this.setState({
+			username: "",
+			password: ""
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.loginUser}>
+					<input
+						type="text"
+						name="username"
+						value={this.state.username}
+						placeholder="username"
+						onChange={this.handleChanges}
+					/>
+
+					<input
+						type="password"
+						name="password"
+						value={this.state.password}
+						placeholder="password"
+						onChange={this.handleChanges}
+					/>
+
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default Login;
